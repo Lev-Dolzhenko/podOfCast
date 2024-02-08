91b7d5c65b565fb8fdecb0a30f78132a38e12bf9
Leo <workspace2707@yandex.ru>
2024-02-08 22:50:23 +0300

    created header, hero, swiper, sponsors blocks

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  loop: true,
+//   autoplay: {
+//     delay: 3000,
+//     disableOnInteraction: false, // Остановить автопрокрутку при взаимодействии пользователя
+//   },
+});
