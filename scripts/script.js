c2cd3d505e65342caeca73e20b902a810e2e873f
Leo <workspace2707@yandex.ru>
2024-02-09 15:41:51 +0300

    created about block

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,22 @@ var swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
   spaceBetween: 20,
   loop: true,
-//   autoplay: {
-//     delay: 3000,
-//     disableOnInteraction: false, // Остановить автопрокрутку при взаимодействии пользователя
-//   },
+  //   autoplay: {
+  //     delay: 3000,
+  //     disableOnInteraction: false, // Остановить автопрокрутку при взаимодействии пользователя
+  //   },
+});
+
+var swiper = new Swiper(".mySwiper_testiomonials", {
+  slidesPerView: 2,
+  spaceBetween: 20,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next-testimonials",
+    prevEl: ".swiper-button-prev-testimonials",
+  },
+  //   autoplay: {
+  //     delay: 3000,
+  //     disableOnInteraction: false, // Остановить автопрокрутку при взаимодействии пользователя
+  //   },
 });
